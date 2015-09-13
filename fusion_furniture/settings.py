67ce7b2032a919bfc409b0f9d9ed50936c4ac92e
Smoliver <samuelolive21@gmail.com>
2015-09-12 23:30:55 -0400

    no longer compressing js

--- fusion_furniture/settings.py
+++ fusion_furniture/settings.py
@@ -101,6 +101,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+COMPRESS_ENABLED = False
+
 STATIC_URL = '/static/fusion_furniture/'
 
 STATIC_ROOT = '/static/fusion_furniture/'
