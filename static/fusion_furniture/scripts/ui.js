d8a20d9acc2a75805442e7abb87620a77839eb16
Sam Oliver <SamO@Sams-MacBook-Pro.local>
2015-10-19 21:24:53 -0500

    new version with images

--- static/fusion_furniture/scripts/ui.js
+++ static/fusion_furniture/scripts/ui.js
@@ -1,88 +1,156 @@
 (function(){
 
-
 var firebase = new Firebase("https://crackling-fire-1294.firebaseio.com/");
 
 
+var Step = function (name, sectionId) {
+	
+	this.getName = function () {
+		return name;
+	} 
+	this.getId = function () {
+		return sectionId;
+	}
+	//hide the section with id = sectionId
+	this.hide =  function () {
+		console.log( "yes" );
+		$( sectionId ).addClass( "left" );
+		setTimeout( function (){
+			$( sectionId ).addClass( "hidden" );
+		}, 50 );
+	}
+	//show the section with id = sectionId
+	this.show = function () {
+		setTimeout( function (){
+			$( sectionId ).removeClass( "hidden" );
+			setTimeout( function (){
+				$( sectionId ).removeClass( "right" );
+			}, 50 );
+		}, 300);
+	}
+}
 
+var ProgramSteps = function (){
+	var ARTICLE_STEP = 0;
+	var FRONT_STEP = 1;
+	var SIDE_STEP = 2;
+	var EMAIL_STEP = 3;
+	var DISPLAY_STEP = 4;
 
-var Steps = function (){
-	this.currentStep = 0;
-	this.stepDisplay = $( "#stepDisplay" );
+	var currentStep = 0;
+
+	var isTable = false;
+	var front = null;
+	var side = null;
+	var email = null;
+
+	var steps = [
+		new Step ("Article of Furniture", "#furniture-types"),
+		new Step ("Front View", "#image-list"),
+		new Step ("Side View", "#image-list"),
+		new Step ("Email", "#email"),
+		new Step ("Fusion Complete", "#display-furniture")
+	];
 
 	this.setStep = function( stepNum ){
-		this.currentStep = stepNum;
-		if (this.stepNum == 0){
-			$( "#furniture-type" ).removeClass( "hidden" );
-		} 
-		else {
-			$( "#furniture-type" ).addClass( "hidden" );
+		if ( steps[currentStep].getId() != steps[stepNum].getId() ){
+			steps [currentStep].hide();
 		}
+		steps [stepNum].show();
+		currentStep = stepNum;
+		$( "#steps > *" ).removeClass( "active" )
+		$( "#steps" ).append("<span class='active'>" + (stepNum + 1) + ". " + steps [stepNum].getName() + "</span>");
+	}
 
-		if (this.currentStep == 3){
-			$( "#complete" ).removeClass( "hidden" );
-			submit ();
-		}
-		else {
-			$( "#complete" ).addClass( "hidden" );
-		}
+	this.start = function (){
+		this.setStep (ARTICLE_STEP);
 	}
 
 	this.setIsTable = function (isTrue){
-		this.isTable = isTrue;
-		this.setStep (1);
+		isTable = isTrue;
+		this.setStep (FRONT_STEP);
 	}
 
 	this.setSide = function (itemText){
-		if (currentStep == 1){
-			this.firstSide = itemText;
-			this.setStep (2);
+		console.log( "currentStep" );
+		if (currentStep == FRONT_STEP){
+			this.front = itemText;
+			this.setStep (SIDE_STEP);
 		}
-		else if (currentStep == 2){
-			this.secondSide = itemText;
-			this.setStep (3);
+		else if (currentStep == SIDE_STEP){
+			this.side = itemText;
+			this.setStep (EMAIL_STEP);
 		}
+	}
+
+	this.setEmail = function (emailTyped){
+		email = emailTyped;
+		this.submit();
+		this.setStep( DISPLAY_STEP );
 	}
 
 	this.submit = function (){
+		var table = isTable ? 1 : 2;
 		var url = "https://www.wolframcloud.com/objects/3e4dcd10-28d8-44cd-9e9e-9851af261134?url1=" 
-		+ firstSide + "&url2=" + secondSide + "&email=" + email + "&table=" + isTable;
+		+ front + "&url2=" + side + "&email=" + email + "&table=" + table;
 		$.get(url, function( data ) {
- 		alert( "Data Loaded: " + data );
-		// decode data here
-});
+	 		alert( "Data Loaded: " + data );
+			// decode data here
+		});
 	}
-
-	this.isTable = 0;
-	this.firstSide = null;
-	this.secondSide = null;
-	this.email = null;
-
 }
 
 
 $( document ).ready( function (){
-	var steps = new Steps ();
-
-	$( ".image-item" ).click( function (){
-		$( this ).addClass( "active" );
-		var image = $( this ).find( "#image" );
-		var rawVal = image.css("background-image");
-		var url = "http://fusion-furniture.azurewebsites.net/" + rawVal.substring(4, rawVal.length-1); 
-		steps.setSide(url);
-	})
+	var programSteps = new ProgramSteps ();
 
 	$( "#chair" ).click( function(){
-		steps.setIsTable (false);
+		programSteps.setIsTable (false);
 	})
 
 	$( "#table" ).click( function(){
-		steps.setIsTable (true);
+		programSteps.setIsTable (true);
 	})
 
+	$( ".image-item" ).click( function (){
+		$( this ).addClass( "active" );
+		var image = $( this ).find( ".image" );
+		var rawVal = image.css("background-image");
+		var url = "http://fusion-furniture.azurewebsites.net/" + rawVal.substring(4, rawVal.length-1); 
 
+		programSteps.setSide(url);
+	})
 
+	var emailDefaultText = "Email to...";
+	
+	var editEmail = $( "#edit-email" );
+	editEmail.text( emailDefaultText );
 
+	editEmail.focus( function (){
+		if (editEmail.text() === emailDefaultText){
+			editEmail.text("");
+		}
+	})
+
+	$( "#edit-email" ).blur( function (){
+		if (editEmail.text() === ""){
+			editEmail.text( emailDefaultText );
+		}
+	})
+
+	$( "#select-email" ).click( function (){
+		var email = editEmail.text();
+		if (email === "" || email === emailDefaultText){
+
+		}
+		
+		programSteps.setEmail();
+	})
+
+	console.log( "1" );
+	programSteps.start ();
 
 });
+
+
 })();
